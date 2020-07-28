d8169446f263cac890152cd3d8fd0600a2c2399c
a b <4b11b4@gmail.com>
2020-07-28 00:00:28 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+//Components
+//import Welcome from './Welcome.js'
+
+ReactDOM.render(
+  <div className="App">
+    <header className="App-header">
+      <img src="/logo.svg" className="App-logo" alt="logo" />
+      <h3>Anton's Website</h3>
+      <p></p>
+    </header>
+  </div>,
+  document.getElementById('root')
+);
